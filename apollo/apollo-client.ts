33395e053777ae9e14d6df23646fb0ce9aa1179a
Teodor Burov <burov96@gmail.com>
2024-07-16 21:48:57 +0300

    Had to link the new vercel link.

--- apollo/apollo-client.ts
+++ apollo/apollo-client.ts
@@ -6,7 +6,7 @@ export const { getClient } = registerApolloClient(() => {
     cache: new InMemoryCache(),
     link: new HttpLink({
       // this needs to be an absolute url, as relative urls cannot be used in SSR
-      uri: 'http://localhost:4000/graphql'
+      uri: 'https://project-oversee-git-main-burov96s-projects.vercel.app/'
       // you can disable result caching here if you want to
       // (this does not work if you are rendering your page with `export const dynamic = "force-static"`)
       // fetchOptions: { cache: "no-store" },
