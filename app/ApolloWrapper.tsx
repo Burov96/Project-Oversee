5fe4ea0419d7b6cb058aa84575d2476957fe90d0
Teodor Burov <burov96@gmail.com>
2024-07-19 14:07:44 +0300

    Updated server configuration and Apollo client URI SECOND TIME

--- app/ApolloWrapper.tsx
+++ app/ApolloWrapper.tsx
@@ -12,7 +12,7 @@ import {
 function makeClient() {
   const httpLink = new HttpLink({
     // this needs to be an absolute url, as relative urls cannot be used in SSR
-    uri: "https://your-heroku-app-name.herokuapp.com/graphql",
+    uri: "https://project-oversee-33d679da664e.herokuapp.com/graphql",
     // you can disable result caching here if you want to
     // (this does not work if you are rendering your page with `export const dynamic = "force-static"`)
     fetchOptions: { cache: "no-store" },
