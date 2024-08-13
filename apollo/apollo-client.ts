f2211066546eb60b6199a4cb6191d146136ef607
Teodor Burov <burov96@gmail.com>
2024-08-13 16:09:32 +0300

    Updated for Vercel deployment

--- apollo/apollo-client.ts
+++ apollo/apollo-client.ts
@@ -5,12 +5,7 @@ export const { getClient } = registerApolloClient(() => {
   return new ApolloClient({
     cache: new InMemoryCache(),
     link: new HttpLink({
-      // this needs to be an absolute url, as relative urls cannot be used in SSR
       uri: '/api/graphql'
-      // uri: 'https://project-oversee-33d679da664e.herokuapp.com//'
-      // you can disable result caching here if you want to
-      // (this does not work if you are rendering your page with `export const dynamic = "force-static"`)
-      // fetchOptions: { cache: "no-store" },
     })
   })
 })
--- app/api/graphql.js
+++ app/api/graphql.js
@@ -1,12 +1,32 @@
 import { ApolloServer } from 'apollo-server-micro';
 import { typeDefs, resolvers } from '../../graphql';
+import Cors from 'micro-cors';
 
-const apolloServer = new ApolloServer({ typeDefs, resolvers });
+const cors = Cors();
+
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers,
+  introspection: true, 
+  playground: true, 
+});
+
+const startServer = apolloServer.start();
+
+export default cors(async function handler(req, res) {
+  if (req.method === 'OPTIONS') {
+    res.end();
+    return false;
+  }
+
+  await startServer;
+  await apolloServer.createHandler({
+    path: '/api/graphql',
+  })(req, res);
+});
 
 export const config = {
   api: {
-    bodyParser: false
-  }
-}
-
-export default apolloServer.createHandler({ path: '/api/graphql' });
+    bodyParser: false,
+  },
+};
